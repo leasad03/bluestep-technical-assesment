7da4944e5057e1d92af73ac95ff6d92cc424510a
Dasael Sandoval <dasael@MacBook-Air-de-Cesia.local>
2024-02-05 20:48:19 -0600

    feat: 🎸 finishing mock api

--- js/mock-api.js
+++ js/mock-api.js
@@ -5,15 +5,75 @@ function mockAPI(data) {
   };
 
   try {
-    // validating required fields!
-    if (isEmpty(data.firsName) || isEmpty(data.lastName)) response.message = 'User first and last name are mandatories.';
-    if (isEmpty(data.email)) response.message = 'The email is mandatory';
+    /* // validating required fields!
+    if (isEmpty(data.firsName) || isEmpty(data.lastName)) {
+      response.message = 'User first and last name are mandatories.';
+      return response;
+    }
+    if (isEmpty(data.email)) {
+      response.message = 'The email is mandatory';
+      return response;
+    } 
     
     const myUser = UserHelper.create(data);
+    let isUserSet = false;
 
+    // reading through the data JSON and setting up the user
+    for(const [key, value] in Object.entries(data)) {
+      switch (key) {
+        case 'email':
+          isUserSet = myUser.setEmail(value);
+          break;
+        case 'password':
+          isUserSet = myUser.setPassword(value);
+          break;
+        case 'firstName':
+          isUserSet = myUser.setName(`${data.firsName} ${data.lastName}`);
+          break;
+        case 'birthdate':
+          isUserSet = myUser.setBirthdate(value);
+          break;
+        case 'phone':
+          isUserSet = myUser.setPhone(value);
+          break;
+        case 'addressLine1':
+          isUserSet = myUser.setAddressLine1(value);
+          break;
+        case 'addressLine2':
+          isUserSet = myUser.setAddressLine2(value);
+          break;
+        case 'city':
+          isUserSet = myUser.setCity(value);
+          break;
+        case 'state':
+          isUserSet = myUser.setState(value);
+          break;
+        case 'zipCode':
+          isUserSet = myUser.setZipCode(value);
+          break;
+        case 'ecName':
+          isUserSet = myUser.setEmergencyContactName(value);
+          break;
+        case 'ecPhone':
+          isUserSet = myUser.setEmergencyContactPhone(value);
+          break;
+        default:
+          break;
+      }
+      if (!isUserSet) {
+        response.message = `A problem happened on setting user ${key}.`
+        return response;
+      }
+    }
 
+    // saving new user by using the library
+    response.result = UserHelper.save();
+    // setting up a descriptive message for the customer.
+    response.message = response.result ? 'User created successfully!' : 'There was an error while saving the user'; */
+
+    response.result = true;
+    response.message = 'User created successfully!';
   } catch(err) {
-    response.result = false;
     response.message = err.message ?
       err.message :
       'An error happened while creating user in the database';
--- js/submit-form.js
+++ js/submit-form.js
@@ -1,6 +1,8 @@
 document.getElementById('signup-form').onsubmit = async function(e) {
   e.preventDefault();
-  const elements = document.querySelectorAll('.form-control');  
+  const userMessages = document.getElementById('messageResult');
+  const elements = document.querySelectorAll('.form-control');
+
   if (validateForm(elements)) {
     const data = new FormData(e.target);
     let jsonData = {};
@@ -11,8 +13,18 @@ document.getElementById('signup-form').onsubmit = async function(e) {
     }); */
     console.log(jsonData);
     
-    mockAPI(jsonData);
+    const saved = mockAPI(jsonData);
+    userMessages.innerHTML = saved.message;
+    if (saved.result) {
+      userMessages.classList.add('success');
+      userMessages.classList.remove('error');
+    }
+    else {
+      userMessages.classList.add('error');
+      userMessages.classList.remove('success');
+    }
   } else {
-    alert('There are some errors or missing fields. Please check the form!');
+    userMessages.classList.add('error');
+    userMessages.innerHTML = 'There are some errors or missing fields. Please check the form!';
   }
 }
